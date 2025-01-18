a4dc14ab029df87cab8a74a91be6dc44e27a1f05
Patrick C <pjcallaghan@Mac.lan>
2025-01-18 10:24:18 -0500

    Updates

---
+++ precache-manifest.27a42c1a6b720a2b782193e3dd450771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae78b682487994e0c53be77450789bd0",
+    "url": "/frog-wisdom/index.html"
+  },
+  {
+    "revision": "a5f14cf51ae21be2aebf",
+    "url": "/frog-wisdom/static/css/main.415956c3.chunk.css"
+  },
+  {
+    "revision": "a0d111f0c241ac24c58b",
+    "url": "/frog-wisdom/static/js/2.ebdf3ede.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/frog-wisdom/static/js/2.ebdf3ede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f14cf51ae21be2aebf",
+    "url": "/frog-wisdom/static/js/main.74e98025.chunk.js"
+  },
+  {
+    "revision": "9adce266881e48aebef2",
+    "url": "/frog-wisdom/static/js/runtime-main.f23d1708.js"
+  }
+]);
